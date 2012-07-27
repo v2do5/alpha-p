a3db8b4606e61bec7fa9493a024f70dcc7d04f54
Vadim Kiselev <v2do5@mail.ru>
2012-07-27 15:55:59 +0400

    qwe

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => 'AKIAIIR7LH5DQKMSQ7CQ',       # required
     :aws_secret_access_key  => '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',       # required
-    :region                 => 'Ireland'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'alpha-p-files'                     # required
 end
