29f8f7d8802aa7a3d199a31c3dd8ed823244c3fe
Vadim Kiselev <v2do5@mail.ru>
2012-07-27 15:15:17 +0400

    S3 added to carrierwave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,5 @@
+CarrierWave.configure do |config|
+  config.s3_access_key_id = "AKIAIIR7LH5DQKMSQ7CQ"
+  config.s3_secret_access_key = "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"
+  config.s3_bucket = "alpha-p-files"
+end
