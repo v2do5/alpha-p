240f1de6dc12e4a6f6cd379530291d5866ab0dbf
Vadim Kiselev <v2do5@mail.ru>
2012-07-27 15:46:23 +0400

    S3 configs altered

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => 'AKIAIIR7LH5DQKMSQ7CQ',       # required
     :aws_secret_access_key  => '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',       # required
-    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    :region                 => 'Ireland'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'Ireland'                     # required
+  config.fog_directory  = 'alpha-p-files'                     # required
 end
