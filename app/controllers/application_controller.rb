f770455b45e00881c1317019955db1f8ecbc24bd
Vadim Kiselev <v2do5@mail.ru>
2012-07-30 19:36:35 +0400

    cancan edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  check_authorization :unless => :devise_controller?
+  check_authorization :unless => :do_not_check_authorization?
+  private
+  def do_not_check_authorization?
+    respond_to?(:devise_controller?) or
+    condition_one? or
+    condition_two?
+  end
 end
